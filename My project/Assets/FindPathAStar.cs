bd1e0dac60fd2d25d6f8b5f9980619a93f75767d
jezeenya <jezeenagaming@gmail.com>
2026-03-24 11:42:43 -0700

    Started and left off on Part 2

---
+++ My project/Assets/FindPathAStar.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PathMarker
+{
+    public MapLocation location;
+    public float G;
+    public float H;
+    public float F;
+    public GameObject marker;
+    public PathMarker parent;
+
+    public PathMarker(MapLocation l, float g, float h, float f, GameObject marker, PathMarker p)
+    {
+        location = l;
+        G = g;
+        H = h;
+        F = f;
+        this.marker = marker;
+        parent = p;
+           
+    }
+
+    public override bool Equals(object obj)
+    {
+        if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+        {
+            return false;
+        }
+        else
+        {
+            return location.Equals(((PathMarker)obj).location);
+        }
+       
+
+    }
+}
+public class FindPathAStar : MonoBehaviour
+{
+    public Maze maze;
+    public Material closedMaterial;
+    public Material openMaterial;
+
+    List<PathMarker> open  = new List<PathMarker>();
+    List<PathMarker> closed = new List<PathMarker>();
+
+    public GameObject start;
+    public GameObject end;
+    public GameObject pathP;
+
+    PathMarker goalNode;
+    PathMarker startNode;
+
+    PathMarker lastPos;
+    bool done = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
